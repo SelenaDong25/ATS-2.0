8feb609d54a9e0f6fbfd5c9094b1fbc1f03cccaf
SelenaDong25 <40871431+SelenaDong25@users.noreply.github.com>
2020-03-02 21:09:44 -0800

    Update Deposit.java

--- Deposit.java
+++ Deposit.java
@@ -1,1 +1,34 @@
+package atmcasestudy;
 
+/**
+ *
+ * @author dx200
+ */
+public class Deposit extends Transaction {
+    private Keypad keypad;
+    private DepositSlot depositSlot;    
+    private int account;
+    private Screen screen;
+    private BankDatabase database;
+    private double depositAmount;
+    
+    public Deposit(int userAccountNumber, Screen atmScreen, BankDatabase atmBankDatabase, Keypad keypad, DepositSlot depositSlot){
+        super(userAccountNumber, atmScreen,atmBankDatabase);
+        this.keypad = keypad;
+        this.depositSlot = depositSlot;
+        account = userAccountNumber;
+        screen = atmScreen;
+        database = atmBankDatabase;
+                
+}
+    
+    @Override    
+    public void execute() {
+        screen.displayMessage("Enter the amount to deposit ");
+        depositAmount = keypad.getInput();
+        depositSlot.isEnvelopeReceived();
+        database.credit(account, depositAmount);
+        screen.displayMessage("You deposited ");
+        screen.displayDollarAmount(depositAmount);
+    }
+}
